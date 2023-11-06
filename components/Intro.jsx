31158dd1b81bc56fbe6cba01721c20ef8d36f0a4
Vishali Sanghishetty <95239214+vishsanghishetty@users.noreply.github.com>
2023-11-06 14:08:56 -0500

    adds css

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Intro() {
   return (
-    <div>
-      <h1 className='p-2 text-blue-600'>Vishali Sanghishetty</h1>
+    <div className='mb-6'>
+      <h1 className='p-4 text-blue-600'>Vishali Sanghishetty</h1>
       <p>
         Full-stack web developer and teacher at Udemy. When not coding or
         preparing a course, I like to play board games, to cook (and eat), or to
--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Card from '../components/Card';
 
 export default function App() {
   return (
-    <div  className='w-96 text-center'>
+    <div  className='mx-14 my-14 w-96 text-center'>
     <h1 className="text-3xl font-bold text-gray-400">
       Dev Starter Profile
       </h1>
